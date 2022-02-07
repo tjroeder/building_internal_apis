e9e4d7e40dc58cbec31afc297af78db899ebab62
Tim Roeder <timothyjroeder@gmail.com>
2022-02-07 16:11:31 -0700

    Feat: add popularity migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_175308) do
+ActiveRecord::Schema.define(version: 2022_02_07_224835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_02_07_175308) do
     t.integer "number_sold"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "popularity"
   end
 
 end
